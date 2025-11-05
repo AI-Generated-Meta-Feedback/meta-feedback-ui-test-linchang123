321b25903c0c2a2a60b8a6c155d07e79912e3cf4
Lin Chang <125248171+linchang123@users.noreply.github.com>
2025-11-05 12:20:23 -0800

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,7 @@
-
+edge case: when input is empty, when input has length 1
+happy case: [{shelf: 5, order: 3}, {shelf: 2, order: 1}, {shelf: 5, order: 2}]
+test books in reverse order and test books already in correct order
+include a scenario where all books are from the same shelf but in random order
+duplicate shelf numbers with different return orders
+checks a large number of books on a single shelf.
 
